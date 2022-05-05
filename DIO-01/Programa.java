2a8f0aa0da20c40eb149a036f41149879a07e441
Crhis Ferreira <ferreiracrhis@gmail.com>
2022-05-05 17:59:05 -0300

    using intelliJ

---
+++ DIO-01/Programa.java
@@ -0,0 +1,14 @@
+public class Programa {
+    public static void main (String[] args) {
+         // boa prática de JAVA
+         int i = 07;
+         int testeDeName = 18;
+     
+     
+         final int j = 10;
+         final int TESTE_NAME = 22;
+     
+         System.out.printIn(i)
+    }
+}
+
